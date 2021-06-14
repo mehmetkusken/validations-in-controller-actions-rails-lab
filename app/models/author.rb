721d2bdecbf1826a0215e3a2cce693df4c0ae7ce
Mehmet <mehmetkusken@gmail.com>
2021-06-13 19:43:18 -0600

    commit tree a04a50b45cf03ade694b5bca66c135cbcd358438

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,4 @@
 class Author < ActiveRecord::Base
-end
+    validates :name, presence: true 
+    validates :email, presence: true, uniqueness: true
+  end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
-end
+    validates_presence_of :title
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validates :content, length: { minimum: 100 }
+  end
